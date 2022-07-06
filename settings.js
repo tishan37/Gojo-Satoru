c6409d7f66dadbba468b11688bf7962c01712195
tishan37 <108791938+tishan37@users.noreply.github.com>
2022-07-06 13:39:31 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,10 +19,10 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
-global.ownername = '🐦Star Boy Nexus'
+global.owner = ['94740551067']
+global.premium = ['94740551067']
+global.ownernomer = '94740551067'
+global.ownername = '🐦Tishan sasidu'
 global.botname = 'Gojo-Satoru'
 global.footer = '©NexusNwInc.'
 global.ig = 'https://github.com/nexusNw'
